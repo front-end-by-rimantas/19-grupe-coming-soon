ff75513764a4095742ede667d97fc2ea8ee26721
Rimantas <belauzas@yahoo.com>
2020-12-02 15:36:50 +0200

    Validation update

--- js/components/form-validator/formValidator.js
+++ js/components/form-validator/formValidator.js
@@ -1,16 +1,21 @@
-import { isValidEmail, isValidName, isValidText } from './validationRules.js';
+import { validation } from './validationRules.js';
 
 function formValidator(selector) {
     const formDOM = document.querySelector(selector);
     const submitBtnDOM = formDOM.querySelector('input[type="submit"]');
+
+    if (!submitBtnDOM) {
+        console.error('ERROR: formoje nerasta input:submit mygtukas.');
+        return false;
+    }
 
     const allInputDOMs = formDOM.querySelectorAll('input:not([type="submit"])');
     const allTextareaDOMs = formDOM.querySelectorAll('textarea');
 
     const allElements = [...allInputDOMs, ...allTextareaDOMs];
 
-    if (!submitBtnDOM) {
-        console.error('ERROR: formoje nerasta input:submit mygtukas.');
+    if (allElements.length === 0) {
+        console.error('ERROR: formoje nerasta nei vieno input ar textarea elementu.');
         return false;
     }
 
@@ -22,28 +27,11 @@ function formValidator(selector) {
             const validationRule = input.dataset.validation;
             const text = input.value;
 
-            if (validationRule === 'name') {
-                const nameError = isValidName(text);
-                if (nameError !== true) {
-                    console.log(nameError);
-                    errorCount++;
-                }
-            }
-
-            if (validationRule === 'email') {
-                const emailError = isValidEmail(text);
-                if (emailError !== true) {
-                    console.log(emailError);
-                    errorCount++;
-                }
-            }
-
-            if (validationRule === 'text') {
-                const textError = isValidText(text);
-                if (textError !== true) {
-                    console.log(textError);
-                    errorCount++;
-                }
+            const validationFunction = validation[validationRule];
+            const error = validationFunction(text);
+            if (error !== true) {
+                console.log(error);
+                errorCount++;
             }
         }
 
